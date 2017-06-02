470cb667ac69fb34164a37252f3f5ec01c9148bd
xorphitus <xorphitus@gmail.com>
2017-06-02 23:24:10 +0900

    add web server for test

--- test/paraq/core_test.clj
+++ test/paraq/core_test.clj
@@ -1,6 +1,28 @@
 (ns paraq.core-test
   (:require [clojure.test :refer :all]
+            [ring.adapter.jetty :as server]
             [paraq.core :refer :all]))
+
+(defonce server (atom nil))
+(def port 3000)
+
+(defn handler [req]
+  {:status 200
+   :body "OK"})
+
+(defn start-server []
+  (when-not @server
+    (reset! server (server/run-jetty handler {:port port :join? false}))))
+
+(defn stop-server []
+  (when @server
+    (.stop @server)
+    (reset! server nil)))
+
+(defn restart-server []
+  (when @server
+    (stop-server)
+    (start-server)))
 
 (deftest a-test
   (testing "FIXME, I fail."
