f9584ca8019fbc07e84e2ac27b26fe80e44a9708
xorphitus <xorphitus@gmail.com>
2017-06-05 23:22:48 +0900

    並列リクエストの雛形っぽい部分を記述

--- src/paraq/core.clj
+++ src/paraq/core.clj
@@ -1,6 +1,9 @@
-(ns paraq.core)
+(ns paraq.core
+  (:require [clj-http.client :refer :all]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn exec [urls]
+  (with-async-connection-pool {:timeout 5 :threads 4 :insecure? true :default-per-route 10}
+    (doseq [url urls]
+      (get url {:async? true}
+           (fn [response] (println response))
+           (fn [exeption] (println exeption))))))
