4352b8b093589fe5a8422599ce4be52c8cb170f1
xorphitus <xorphitus@gmail.com>
2017-06-20 00:37:21 +0900

    ロガーの生成部分を外出し

--- src/paraq/core.clj
+++ src/paraq/core.clj
@@ -11,13 +11,19 @@
   (client/with-async-connection-pool {:timeout 5 :threads 4 :insecure? true :default-per-route 10}
     (doseq [url urls]
       (client/get url {:async? true}
-           (fn [response] (async/put! chan (fmt url response)))
-           (fn [exeption] (async/put! chan (fmt url (.getData exeption))))))))
+                  (fn [response] (async/put! chan [url response]))
+                  (fn [exeption] (async/put! chan [url (.getData exeption)]))))))
+
+(defmacro gen-logger [formatter chan]
+  `(async/thread
+     (while true
+       (let [data# (async/<!! ~chan)
+             url# (first data#)
+             response# (last data#)]
+         (println (~formatter url# response#))))))
 
 (defn exec-with-logging [urls]
   (let [chan (async/chan)]
     (do
-      (async/thread
-        (while true
-          (println (async/<!! chan))))
+      (gen-logger fmt chan)
       (exec urls chan))))
